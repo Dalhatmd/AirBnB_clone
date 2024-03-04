1e99c52a80b8a852fddaabef2fe796c82f4086b1
Dalhatmd <umarwalid4055@gmail.com>
2024-03-04 20:35:32 +0100

    Made only dictionary time obiects strings

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,8 @@ class BaseModel:
     def __init__(self):
         """ initialiser for BaseModel """
         id = str(uuid.uuid4())
-        created_at = datetime.now().isoformat()
-        updated_at = datetime.now().isoformat()
+        created_at = datetime.now()
+        updated_at = datetime.now()
         self.id = id
         self.created_at = created_at
         self.updated_at = updated_at
@@ -20,10 +20,12 @@ class BaseModel:
 
     def save(self):
         """ Updates the updated_at value"""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns a dictionary representation of the class"""
         class_dict = self.__dict__
         class_dict["__class__"] = __class__.__name__
+        class_dict["created_at"] = self.created_at.isoformat()
+        class_dict["updated_at"] = self.updated_at.isoformat()
         return class_dict
